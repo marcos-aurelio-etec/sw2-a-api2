ca2050fa52073faceaf9dd59544119185339644e
marcos-aurelio-etec <marcosllima.marcia@gmail.com>
2024-09-13 12:23:26 +0000

    legaal15/09/24

---
+++ app/src/main/java/application/repository/PessoaRepository.java
@@ -0,0 +1,11 @@
+package application.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import application.model.Pessoa;
+
+public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
+
+    
+}
